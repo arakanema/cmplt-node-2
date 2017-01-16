7615a5afb409b4836b5fe709fd74be63e06ddd8b
Masahiro Arakane <arakane.masahiro@gmail.com>
2017-01-17 00:09:28 +0900

    5-44 done

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,14 +3,22 @@ const hbs = require('hbs');
 
 app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website!',
-    currentYear: new Date().getFullYear()
   });
 });
 
@@ -18,7 +26,6 @@ app.get('/about', (req, res) => {
   // res.send('About Page');
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
