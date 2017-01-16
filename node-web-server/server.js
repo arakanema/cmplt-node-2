1b354fb25fdbc849b5d3e53b83806c9a15ea3703
Masahiro Arakane <arakane.masahiro@gmail.com>
2017-01-16 23:36:05 +0900

    5-43 done

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,21 +1,25 @@
 const express = require('express');
+const hbs = require('hbs');
 
 app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  res.send({
-    name: 'Masahiro',
-    likes: [
-      'Biking',
-      'Cities'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website!',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  // res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get('/bad', (req, res) => {
