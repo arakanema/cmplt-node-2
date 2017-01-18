122a32eae3c84e73fdd7bce5041333929833a5b0
Masahiro Arakane <arakane.masahiro@gmail.com>
2017-01-19 00:09:52 +0900

    6-54 done. test for async methods

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -8,10 +8,24 @@ it('should add two numbers', () => {
   expect(res).toBe(44).toBeA('number');
 });
 
+it('should asyncAdd two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
+});
+
 it('shoud square a number', () => {
   var res = utils.square(8);
 
   expect(res).toBe(64).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(3, (res) => {
+    expect(res).toBe(9).toBeA('number');
+    done();
+  });
 });
 
 // should verify first and last names are set
